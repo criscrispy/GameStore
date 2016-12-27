3bb32fcda65d10bdd4c6349547fda8ddf537bed0
Mekbib Awono <mekbib.awono@aalto.fi>
2016-12-28 00:36:25 +0200

    added list of urls and views to be implemented. created empty html pages

--- gamestore/urls.py
+++ gamestore/urls.py
@@ -4,5 +4,91 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^accounts/profile', views.profile, name='profile')
+    url(r'^accounts/profile', views.profile, name='profile'),
+
+    #   /games
+    #   /games?order=rating|created|price|popularity
+    #   shows list of games. could be landing page after login
+    url(r'^games$', views.games, name='games.list'),
+
+    #   /games/user/1234
+    #   games the user bought
+    url(r'^games/user/(?P<user_id>[0-9]{2,})', views.game_sale, name='games.sale'),
+
+    #   /games/supermario OR /games/1234 (only with ID)??
+    #   the detail view of one of the games, one can play/buy/like from that view
+    url(r'^games/(?P<game_name>\w+)', views.game_detail, name='games.detail'),
+
+    #   /games/supermario/play
+    #   end point to register the scores of user when they perform the "play action"
+    url(r'^games/(?P<game>\w+)/play', views.game_play, name='games.play'),
+
+    #   /games/supermario/buy
+    #   end point to buy a game : adds the game to user's bought-list
+    url(r'^games/(?P<game>\w+)/buy', views.game_buy, name='games.buy'),
+
+    #   /games/supermario/like (add to favorites) ??
+    #   end point to like a game : adds the game to user's liked-list
+    url(r'^games/(?P<game>\w+)/like', views.game_like, name='games.like'),
+
+    #   /categories
+    #   shows the list of categories.
+    url(r'^categories', views.categories, name='categories.list'),
+
+    #   /categories/sport
+    #   shows the list of games under given category
+    url(r'^categories/(?P<category>\w+)', views.category_detail, name='categories.detail'),
+
+
+    #   /profile
+    #   url(r'^', views., name=''),
+
+    #   /user/123
+    #   url(r'^', views., name=''),
+
+    #   /user/123/edit
+    #   url(r'^', views., name=''),
+
+
+    #   /history/1234
+    #   history of userid=1234 interms of games they played
+    url(r'^history/(?P<user_id>[0-9]{2,})', views.user_history, name='user.history'),
+
+    #   /upload
+    #   view to show the upload form
+    url(r'^upload', views.upload, name='games.upload'),
+
+    #   /uploads/1234
+    #   games user 1234 uploaded
+    url(r'^uploads/(?P<user_id>[0-9]{2,})', views.uploads, name='games.uploads'),
+
+    #   /uploads/supermario OR /uploads/1234
+    #   details of one of the uploaded games : visible to the uploader
+    url(r'^uploads/(?P<game_id>[0-9]{2,})', views.upload_detail, name='games.upload.detail'),
+
+    #   /uploads/supermario/stats OR /uploads/1234/stats
+    #   view the stats of the uploaded game with gameId=1234
+    url(r'^uploads/(?P<game_id>[0-9]{2,})/stats', views.upload_stat, name='games.upload.stat'),
+
+    #   /uploads/supermario/delete
+    #   edit one of the uploaded games
+    #   the post request could save the updated info
+    url(r'^uploads/(?P<game_id>[0-9]{2,})/edit', views.upload_edit, name='games.upload.edit'),
+
+    #   /uploads/supermario/edit
+    #   edit one of the uploaded games (change picture, name etc)
+    url(r'^uploads/(?P<game_id>[0-9]{2,})/delete', views.upload_delete, name='games.upload.delete'),
+
+    #   /publishers
+    #   /publishers?order=rating|created
+    #   list of publishers
+    url(r'^publishers', views.publishers, name='publishers.list'),
+
+    #   /publishers/nintendo OR /publishers/12345
+    #   details of one of the publishers, list of games they published etc
+    url(r'^publishers/(?P<user_id>[0-9]{2,})', views.publisher_detail, name='publisher.detail'),
+
+
+    #   /search
+    url(r'search/(?P<keyword>\w+)', views.search, name='search')
 ]
--- gamestore/views.py
+++ gamestore/views.py
@@ -1,6 +1,6 @@
 import logging
-
 from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 
@@ -29,3 +29,89 @@ def profile(request):
     """
     context = {}
     return render(request, 'accounts/profile.html', context)
+
+
+def games(request):
+    return render(request, "gamestore/game.html", {})
+
+
+def game_detail(request, game_name):
+    game = {
+        'title': game_name,
+        'price': '0.99'
+    }
+
+    return render(request, "gamestore/game_description.html", {'game': game})
+
+
+def game_play(request, game_name):
+    return None
+
+
+def game_buy(request, game_name):
+    return None
+
+
+def game_like(request, game_name):
+    return None
+
+
+def categories(request):
+    return render(request, "gamestore/categories.html", {})
+
+
+def category_detail(request, category_name):
+    context = {
+        'category_name': category_name
+    }
+
+    return render(request, "gamestore/category_detail.html", context)
+
+
+def game_sale(request, user_id):
+    return render(request, "gamestore/game_sale.html", {})
+
+
+def user_history(request, user_id):
+    return None
+
+
+def uploads(request, user_id):
+    return None
+
+
+def upload(request):
+    return None
+
+
+def upload_detail(request, game_id):
+    return None
+
+
+def upload_stat(request, game_id):
+    return None
+
+
+def upload_edit(request, game_id):
+    return None
+
+
+def upload_delete(request, game_id):
+    return None
+
+
+def publishers(request):
+    return render(request, "gamestore/publishers.html", {})
+
+
+def publisher_detail(request, user_id):
+    publisher = {
+        'user_id': user_id
+    }
+
+    return render(request, "gamestore/publisher_detail.html", {'publisher': publisher})
+
+
+def search(request, keyword):
+    return None
+
