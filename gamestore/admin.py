b1015c4596ef398e99102af656ae1dd6045996af
Mekbib Awono <mekbib.awono@aalto.fi>
2016-12-28 16:05:36 +0200

    Registered models so that they can be accessed from admin panel

--- gamestore/admin.py
+++ gamestore/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+admin.site.register(Game)
+admin.site.register(Profile)
+admin.site.register(Score)
+admin.site.register(GameSale)
