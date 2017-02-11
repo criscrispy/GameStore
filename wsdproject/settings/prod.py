8e3314deda8b05b6639e2fb3b69ddb90d24505b8
Julia Sulina <julia.sulina@aalto.fi>
2017-02-11 15:36:21 +0200

    static

--- wsdproject/settings/prod.py
+++ wsdproject/settings/prod.py
@@ -5,6 +5,3 @@ DEBUG = False
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- wsdproject/wsgi.py
+++ wsdproject/wsgi.py
@@ -14,5 +14,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wsdproject.settings.prod")
 
 application = get_wsgi_application()
-
 application = DjangoWhiteNoise(application)
