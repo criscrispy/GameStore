9687c9c265ad66cdc28c3c06290ceedd17f7b3d3
Julia Sulina <julia.sulina@aalto.fi>
2017-02-11 15:29:24 +0200

    fix static files on heroku

--- wsdproject/settings/prod.py
+++ wsdproject/settings/prod.py
@@ -5,5 +5,6 @@ DEBUG = False
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
