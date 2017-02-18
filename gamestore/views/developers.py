e3eb65eba991f3bd824206613753b6139a89560b
Julia Sulina <julia.sulina@aalto.fi>
2017-02-18 12:34:09 +0200

    Sale statistics for developers in chart

--- gamestore/views/developers.py
+++ gamestore/views/developers.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count, Sum
+from django.db.models.functions import TruncDate
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 from gamestore.forms import GameForm
-from gamestore.models import Game
+from gamestore.models import Game, GameSale
 from gamestore.views.accounts import profile
+from chartit import DataPool, Chart
 
 
 def uploads(request, user_id):
@@ -58,3 +61,48 @@ def upload_edit(request, game_id):
 
 def upload_delete(request, game_id):
     return HttpResponse()
+
+
+@login_required
+def sale_stat(request):
+    if not request.user.userprofile.is_developer():
+        return profile(request)
+    user = request.user
+    sale = GameSale.objects.filter(game__publisher=user).annotate(date_no_time=TruncDate('date')).values('date_no_time').annotate(
+        amount=Count('date_no_time')).annotate(profit=Sum('game__price'))
+    sale_chart = create_chart(sale)
+    # Step 3: Send the chart object to the template.
+    return render(request, "gamestore/sale_chart.html", {'salechart': sale_chart})
+
+
+def create_chart(sale):
+    # http://chartit.shutupandship.com/docs/#how-to-create-charts
+    # Step 1: Create a DataPool with the data we want to retrieve.
+    sale_data = \
+        DataPool(
+            series=
+            [{'options': {
+                'source': sale},
+                'terms': [
+                    'amount',
+                    'date_no_time',
+                    'profit']}
+            ])
+    # Step 2: Create the Chart object
+    sale_chart = Chart(
+        datasource=sale_data,
+        series_options=
+        [{'options': {
+            'type': 'line',
+            'stacking': False},
+            'terms': {
+                'date_no_time': [
+                    'amount', 'profit']
+            }}],
+        chart_options=
+        {'title': {
+            'text': 'Games bought'},
+            'xAxis': {
+                'title': {
+                    'text': 'Date'}}})
+    return sale_chart
