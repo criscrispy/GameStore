1adc710f0e4aa1c34ae0c8e8cbe05bfb5bae2d6a
Julia Sulina <julia.sulina@aalto.fi>
2017-02-19 16:20:19 +0200

    validate json

--- gamestore/constants.py
+++ gamestore/constants.py
@@ -35,6 +35,7 @@ GAME_SCORE_INVALID = "Validation error: score received from the game invalid"
 GAME_STATE_INVALID = "Validation error: state received from the game invalid"
 REQUEST_PARAMETER_MISSING = "Validation error: request parameter missing"
 GAME_INVALID = "Validation error: game not found"
+NOT_A_VALID_JSON = "Validation error: not valid json"
 
 PAYMENT_WAS_CANCELLED = 'Payment was cancelled'
 ERROR_PERFORMING_PAYMENT = 'Error when performing payment'
